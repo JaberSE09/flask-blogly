2c5ba772d2cc24498717dab146e36b834fc5ded2
Sajid Jaber <sajidjaber@yahoo.com>
2024-03-14 13:06:29 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+"""Blogly application."""
+
+from models import db, connect_db
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
